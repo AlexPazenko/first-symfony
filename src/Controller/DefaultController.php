5e24877c080a5bf8e32f38aa407e9b7949c2c1e1
Alex_Pazenko <apazenko.pazenko@ek-dt-020>
2021-10-22 18:26:44 +0300

    Work in project

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,40 +2,46 @@
 
 namespace App\Controller;
 
+/*use http\Cookie;*/
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Services\GiftsService;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
 
-    /*public function __construct(GiftsService $gifts)
+    public function __construct($logger)
     {
-        $gifts->gifts = ['a', 'b', 'c', 'd'];
-    }*/
+
+    }
     /**
-     * @Route("/default/", name="default")
+     * @Route("/", name="default")
      */
-    public function index(GiftsService $gifts)
+    public function index(GiftsService $gifts, Request $request, SessionInterface $session)
     {
-        /*$users = ['Adam', 'Robert', 'John', 'Susan'];*/
+
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
-        /*$entityManager = $this->getDoctrine()->getManager();
-        $user = new User;
-        $user->setName('Adam');
-        $user2 = new User;
-        $user2->setName('Robert');
-        $user3 = new User;
-        $user3->setName('John');
-        $user4 = new User;
-        $user4->setName('Susan');
-        $entityManager->persist($user);
-        $entityManager->persist($user2);
-        $entityManager->persist($user3);
-        $entityManager->persist($user4);
-        exit($entityManager->flush());*/
+
+        if (!$users) {
+            throw $this->createNotFoundException('The user do not exist');
+        }
+
+        $this->addFlash(
+            'notice',
+            'Your changes were saved!'
+        );
+
+        $this->addFlash(
+            'warning',
+            'Your changes were saved!'
+        );
 
 
         return $this->render('default/index.html.twig', [
@@ -43,10 +49,6 @@ class DefaultController extends AbstractController
             'users' => $users,
             'random_gift' => $gifts->gifts,
         ]);
-        /*return $this->Json(['username'=>'john.doe']);*/
-        /*return new Response("Hello! $name");*/
-
-        /*return $this->redirectToRoute('default2');*/
     }
 
     /**
@@ -56,4 +58,101 @@ class DefaultController extends AbstractController
     {
         return new Response('I am from default2 route!');
     }*/
+
+    /**
+     * @Route ("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
+     */
+
+    public function index2()
+    {
+        return new Response('Optional parameters in url and requirements for parameters');
+    }
+
+
+    /**
+     * @Route (
+     *
+     *     "/articles/{_locale}/{year}/{slug}/{category}",
+     *     defaults={"category": "computers"},
+     *     requirements={
+     *       "_locale": "en|fr",
+     *       "category":"computers|rtv",
+     *       "year": "\d+"
+     *     }
+     * )
+     */
+
+    public function index3()
+    {
+        return new Response('An advanced route example');
+    }
+
+    /**
+     * @Route ({
+     *     "nl": "/over-ons",
+     *     "en": "/about-us"
+     *     }, name="about_us"
+     *     )
+     */
+
+    public function index4()
+    {
+        return new Response('Translated routes');
+    }
+
+    /**
+     *
+     * @Route("generate-url/{param?}", name="generate_url")
+     *
+     */
+
+    public  function generate_url()
+    {
+        exit($this->generateUrl(
+            'generate_url',
+            array('param' => 10),
+            UrlGeneratorInterface::ABSOLUTE_URL
+        ));
+    }
+
+    /**
+     * @Route ("/download")
+     */
+    public function download()
+    {
+        $path = $this->getParameter('download_directory');
+        return $this->file($path.'small_bird.jpg');
+    }
+
+
+    /**
+     * @Route("/redirect-test")
+     */
+    public function redirectTest()
+    {
+        return $this->redirectToRoute('generate_url');
+    }
+
+    /**
+     * @Route ("/forwarding-to-controller/{param?}")
+     */
+    public function forwardingToConroller()
+    {
+        $response = $this->forward(
+            'App\Controller\DefaultController::methodToForwardTo',
+            array('param' => '25')
+        );
+        return $response;
+    }
+
+
+    /**
+     * @Route("/url-to-forward-to/{param?}", name="rout_to_forward_to")
+     */
+
+    public function methodToForwardTo($param)
+    {
+       exit('Test controller forwarding - '.$param);
+    }
 }
+
