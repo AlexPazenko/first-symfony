5abe6340e05e4f8ef7ad2ff17d34e2967d31c1b2
Alex_Pazenko <apazenko.pazenko@ek-dt-020>
2021-12-03 18:21:07 +0200

    Exercise 115

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,19 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
 {
     /**
-     * @Route("/to/do/list", name="to_do_list")
+     * @Route("/to-do-list", name="to_do_list")
      */
     public function index(): Response
     {
+        $tasks = $this->getDoctrine()->getRepository(Task::class)->findAll();
         return $this->render('to_do_list/index.html.twig', [
-            'controller_name' => 'ToDoListController',
+            'tasks' => $tasks
         ]);
+    }
+
+
+    /**
+     * @Route("/create-task", name="create-task" methods={"POST"})
+     */
+    public function create(Request $request): Response
+    {
+
+        $title = trim($request->request->get('title'));
+        if (empty($title)) {
+            return $this->redirectToRoute('to_do_list');
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $task = new Task();
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
+
+    }
+
+
+
+    /**
+     * @Route("/switch-status/{id}", name="switch-status" , requirements={"id"="\d+"}))
+     */
+    public function switchStatus($id): Response
+    {
+        exit('to do: switch status of the task!' . $id);
+    }
+
+
+    /**
+     * @Route("/delete/{id}", name="task_delete" , requirements={"id"="\d+"}))
+     */
+    public function deleteTask($id): Response
+    {
+        exit('to do: delete a task with the id of !' . $id);
     }
 }
